ea0697f13e1f44f46bdf508cf13aba6aab58ecef
Rahul kumar <rahulparida933@gamil.com>
2021-01-22 14:58:09 +0530

    cookies and sessions added with passport.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
-// const session = require('express-session');
-// const passport = require('passport');
-// const passportLocalMongoose = require('passport-local-mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const saltRounds = 10
 
@@ -13,14 +12,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// app.use(session({
-//     secret: "our little secret",
-//     resave: false,
-//     saveUninitialized: false
-// }));
+app.use(session({
+    secret: "our little secret",
+    resave: false,
+    saveUninitialized: false
+}));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true, useUnifiedTopology:true}).then(() => {
     console.log('connection established with db');
@@ -29,17 +28,32 @@ mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true, useU
 });
 mongoose.set('useCreateIndex', true);
 
-const secret = 'thisismysecret'
 
 const userSchema = new mongoose.Schema({
     password:String,
     email: String
 });
 
-// userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.get('/secrets', (req, res) => {
+    if(req.isAuthenticated()) { 
+      res.render('secrets')
+    }else {
+      res.redirect('/login')
+    }
+})
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.get('/register', (req, res) => {
     res.render('register')
@@ -51,35 +65,35 @@ app.get('/login', (req, res) => {
 
 app.post('/register', (req, res) => {
     
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) =>{
-
-    let newUser = new User({
-        password: hash,
-        email: req.body.username
-    }) 
-    newUser.save()
-    .then(() => {
-        res.render('secrets')
-    }).catch((e) => {
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        if(err) { 
         res.redirect('/register')
+        }else {
+         passport.authenticate('local') (req, res, () => {
+            res.redirect('/secrets');
+         })
+        }
     })
-    if(err) throw err
-    })
+  
 })
 
 app.post('/login', (req, res) => {
-    const userName = req.body.username
-    const pass = req.body.password
-
-    User.findOne({email: userName}, (e, foundUser) => {
-        if(e) throw e
-        if(foundUser) 
-        bcrypt.compare(pass, foundUser.password, (err, result) => {
-            if(result)
-            res.render('secrets')
-            res.redirect('/login')
-        })    
+   
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, (err) => {
+        if(err) throw err
+        passport.authenticate('local')(req,res, () => {
+            res.redirect('/secrets')
+        })
     })
+})
+
+app.get('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/')
 })
 
 // CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
@@ -87,10 +101,6 @@ app.post('/login', (req, res) => {
  
 // passport.serializeUser(User.serializeUser());
 // passport.deserializeUser(User.deserializeUser());
-
-// app.get('/', (req, res) => {
-//     res.render('home');
-// });
 
 // app.get('/login', (req, res) => {
 //     res.render('login');
